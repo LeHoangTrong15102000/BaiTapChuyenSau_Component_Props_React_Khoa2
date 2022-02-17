7d1a6a05ac5876d2a4691a8be2001021135ef664
Le Hoang Trong <langtupro0456@gmail.com>
2022-02-17 17:03:29 +0700

    Complete Exercise Props

--- src/ProductList/ProductItem.jsx
+++ src/ProductList/ProductItem.jsx
@@ -1,7 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class ProductItem extends Component {
   render() {
-    return <div>ProductItem</div>;
+    // Nếu khách hàng có thêm bớt button hay gì đó thì chỉ cần vào đây mà sửa thôi
+    // Làm front end quan trọng là giao diện
+
+    let { image , name, price , description} = this.props.dataProductItem
+    return (
+      <div className="card text-left" style={{ width: "350px" }}>
+      <img src={image} alt={image} />
+      <div className="card-body bg-primary">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">Giá tiền: {price}</p>
+        <button className="w3-button w3-black text-white">
+          Add to cart
+          <i class="fa fa-shopping-cart"></i>
+        </button>
+      </div>
+    </div>
+   );
   }
 }
+
+{/* Nếu mà để như vậy thì cái đôi giầy cũng không chiếm hết được cái card, nên chúng ta sẽ canh cái style ngay chỗ cái card */}
--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
-import ProductItem from './ProductItem';
+import React, { Component } from "react";
+import ProductItem from "./ProductItem";
 
 export default class ProductList extends Component {
+  // Kiểm tra file json
+
+  // Từ cái mảng trên thì sẽ xây dựng cái ứng dụng bán giầy
+  //   bài này sẽ render dữ liệu thông qua props
+
+  renderProductItem = () => {
+    // để render ra được component thì chúng ta sẽ sử dụng đối tượng là props luôn
+    return this.props.arrProduct.map((item, index) => {
+
+      // Trong vòng lặp cứ mỗi lần duyệt phần tử thì nó sẽ tạo ra 1 components
+      return (
+        // Nếu mà chia col-4 ở dưới giao diện thì các đôi giày nó sẽ không có khoảng trống và nó full luôn phần div của thẻ, thì thằng bên trong nó sễ không chiếm hết thằng bên ngoài
+        // do bootstrap có những cái div column nó có margin lẫn nhau
+
+        // ProductItem phải yêu cầu tổ chức riêng luôn để sau này nó còn sử dụng riêng cho trang tìm kiếm hay trang nào đó
+
+
+        // Truyền object trong vòng lặp luôn
+        // Product sẽ truyền props qua thằng con của nó
+        <div
+          className="col-4 mt-3 w3-container w3-center w3-animate-zoom"
+          key={index}
+        >
+          <ProductItem dataProductItem={item} />
+        </div>
+      );
+    });
+  };
   render() {
-    return (
-      <div>
-        <ProductItem />
-      </div>
-    );
+    return <div className="row">{this.renderProductItem()}</div>;
   }
 }
