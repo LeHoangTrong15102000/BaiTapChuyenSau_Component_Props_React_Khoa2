79cf86deb38c5c4a662f25522703e6b132f00061
Le Hoang Trong <langtupro0456@gmail.com>
2022-02-17 11:22:41 +0700

    Add ProductList file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import MultiComponent from './React _Multi_Component/MultiComponent'
+import MultiComponent from './React _Multi_Component/MultiComponent';
 import Props from './Props/Props';
+import ProductList from './ProductList/ProductList';
+import dataJson from './Data/data.json';
 
 function App() {
   return (
     <div className="App">
       {/* <MultiComponent /> */}
 
-      <Props  />
+      {/* <Props  /> */}
+
+      <ProductList arrProduct={dataJson} />
     </div>
   );
 }
