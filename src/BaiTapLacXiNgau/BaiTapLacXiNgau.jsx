e3a47a122bd02f0eb63702d15aa371c66191ba83
Le Hoang Trong <langtupro0456@gmail.com>
2022-02-26 15:16:42 +0700

    add Redux LacXiNgau file

--- src/BaiTapLacXiNgau/BaiTapLacXiNgau.jsx
+++ src/BaiTapLacXiNgau/BaiTapLacXiNgau.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 export default class BaiTapLacXiNgau extends Component {
+  // Ôn tập redux lại một lần nữa
   render() {
     return (
       <div>BaiTapLacXiNgau</div>
---
+++ src/ShoppingCartRe/ProductListRe.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ProductListRe extends Component {
+  render() {
+    return (
+      <div>ProductListRe</div>
+    )
+  }
+}
