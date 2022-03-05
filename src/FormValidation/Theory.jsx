3590cf579c5be2f6c4dd785964b948330af2ea34
Le Hoang Trong <langtupro0456@gmail.com>
2022-03-05 17:11:51 +0700

    Complete get value input from InputControl

--- src/FormValidation/Theory.jsx
+++ src/FormValidation/Theory.jsx
@@ -5,3 +5,18 @@
      * B1 : copy demo mẫu vào ứng dụng và fix cho chạy được
      * B2 : Tiến hành edit lại
      */
+
+// Lấy thông tin từ thẻ input như thế nào
+// Cách thực hiện -> là gán nguồn dữ liệu đó vào trỏng nút control luôn để khi mà người dùng nhập vào thì tự động cái state đó thay đổi cho mình luôn -> thì khi đó mình sẽ lấy được thông tin người dùng nhập vào
+// Thay vì dung DOM gán riêng lẻ trên từng sự kiện onClick của form -> thì bây giờ gán trực tiếp sự kiện binding trên từng thẻ luôn
+
+/**
+ * Cách thức để lấy thông tin  
+ *      - Đặt tên thuộc tính trong state trùng với control là "name" trong thẻ input luôn
+ *      - Người dùng thay đổi ở sự kiện onChange của thẻ input, chúng ta muốn khi mà nhập liệu thì nó sẽ binding lên trang form cho chúng ta, thì chúng ta nhập vào cái gì nó sẽ binding ra cái đó
+ * 
+ *      - Viết một hàm để lấy value của từng ô input
+ *     
+ */
+
+// *************************** Bài học tiếp validate cho ô input
--- src/FormValidation/UserProfie/UserProfile.jsx
+++ src/FormValidation/UserProfie/UserProfile.jsx
@@ -3,6 +3,40 @@ import { flushSync } from "react-dom";
 import "./UserProfile.css";
 
 export default class UserProfile extends Component {
+
+  state = {
+    firstName: '',
+    lastName: '',
+    userName: '',
+    email: '',
+    passWord: '',
+    passWordConfirm: '',
+  }
+
+  // Viết hàm lấy value của thẻ input
+  // handleChangeValue = (name , value) => {
+  //   // thử xem -> name nào thì sẽ trả về value đó
+  //   console.log(name,value)
+  //   // Thì ở đây thằng nào truyền giá trị vào sẽ ứng với cái name của thằng đó
+  //   this.setState({
+  //     // lấy động về mặt thuộc tính
+  //     [name]: value, // dùng ES6, ứng với name nào thì value tương ứng của name đó
+  //   }, () => {
+  //     console.log(this.state);// xem sau khi thực hiện setState thì giá trị của state là như nào
+  //   })
+  // }
+
+  // Thay vì phải viết liên tực ở trong form input ta đem ra thành một cái hàm -> Viết như vậy thì nó sẽ gắn ngọn và clean code hơn nhiều
+  handleChangeValue = (event) => {
+    let {name , value } = event.target  
+    this.setState({
+      // setState nó lại bằng cách là giá trị của thằng input nào thì được nhập và ô của input đó
+      [name]: value,
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
   render() {
     // Đặt thêm thuộc tính name trong ô input để người dùng biết là đang gõ vào ô input nào
     return (
@@ -20,7 +54,7 @@ export default class UserProfile extends Component {
           <div className="row">
             <div className="col-sm-6">
               <div className="group">
-                <input type="text" name="firstName" required />
+                <input type="text" name="firstName" required onChange={this.handleChangeValue} />
                 <span className="highlight" />
                 <span className="bar" />
                 <label>firstName</label>
@@ -29,7 +63,7 @@ export default class UserProfile extends Component {
 
             <div className="col-sm-6">
               <div className="group">
-                <input type="text" name="lastName" required />
+                <input type="text" name="lastName" required onChange={this.handleChangeValue} />
                 <span className="highlight" />
                 <span className="bar" />
                 <label>lastName</label>
@@ -40,7 +74,7 @@ export default class UserProfile extends Component {
           <div className="row">
             <div className="col-sm-12">
               <div className="group">
-                <input type="text" name="userName" required />
+                <input type="text" name="userName" required onChange={this.handleChangeValue} />
                 <span className="highlight" />
                 <span className="bar" />
                 <label>userName</label>
@@ -51,7 +85,7 @@ export default class UserProfile extends Component {
           <div className="row">
             <div className="col-sm-12">
               <div className="group">
-                <input type="text" name="email" required />
+                <input type="text" name="email" required onChange={this.handleChangeValue}/>
                 <span className="highlight" />
                 <span className="bar" />
                 <label>Email</label>
@@ -61,7 +95,7 @@ export default class UserProfile extends Component {
           <div className="row">
             <div className="col-sm-6">
               <div className="group">
-                <input type="password" name="passWord" required />
+                <input type="password" name="passWord" required onChange={this.handleChangeValue} />
                 <span className="highlight" />
                 <span className="bar" />
                 <label>passWord</label>
@@ -70,7 +104,7 @@ export default class UserProfile extends Component {
 
             <div className="col-sm-6">
               <div className="group">
-                <input type="password" name="passWordConfirm" required />
+                <input type="password" name="passWordConfirm" required onChange={this.handleChangeValue}/>
                 <span className="highlight" />
                 <span className="bar" />
                 <label>passWordConfirm</label>
